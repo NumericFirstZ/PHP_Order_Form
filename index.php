6922c3713dd2ebe09309114765364689ce79d07a
NumericFirstZ <firsttanapol2000@gmail.com>
2022-02-17 00:17:15 +0700

    update UI

--- index.php
+++ index.php
@@ -9,38 +9,63 @@
            <div class="container">  
                 <br />  
                 <br />  
-                <h2 align="center">Items</h2>  
-                <div class="form-group">  
+                <h2 align="center">Order</h2>
+                <div class="form-group">
                      <form name="add_name" id="add_name">  
-                          <div class="table-responsive">  
-                               <table class="table table-bordered" id="dynamic_field">  
-                                    <tr>  
-                                         <td><input type="text" name="product[]" placeholder="Product Name" class="form-control name_list" /></td> 
-                                         <!-- <td><input type="text" name="quantity[]" placeholder="Product Quantity" class="form-control name_list" /></td> -->
+                          <div class="table-responsive">
+                               <table class="table table-bordered" id="dynamic_field">
+                                    <tr>
+                                        <p>Name
+                                             <select name="name" class="form-control name_list">
+                                                  <option value="1">1</option>
+                                                  <option value="2">2</option>
+                                                  <option value="3">3</option>
+                                                  <option value="4">4</option>
+                                             </select>
+                                        </p>
+                                        <p>Location
+                                             <select name="location" class="form-control name_list">
+                                                  <option value="1">1</option>
+                                                  <option value="2">2</option>
+                                                  <option value="3">3</option>
+                                                  <option value="4">4</option>
+                                             </select>
+                                        </p>
+                                        <p>Date to be delivered
+                                             <input type="date" name="product[]" placeholder="Product Name" class="form-control name_list" />
+                                        </p>
+                                   </tr>
+
+                                   <h2 align="center">Items</h2>
+                                    <tr>
+                                         <td>
+                                             <input type="text" name="product[]" placeholder="Product Name" class="form-control name_list" />
+                                        </td>
+
                                         <td>
                                              <select name="quantity" class="form-control name_list">
-
                                                   <option value="1">1</option>
                                                   <option value="2">2</option>
                                                   <option value="3">3</option>
                                                   <option value="4">4</option>
                                              </select>
                                         </td>
+
                                         <td>
                                              <select name="grams" class="form-control name_list">
-
                                                   <option value="1">1</option>
                                                   <option value="2">2</option>
                                                   <option value="3">3</option>
                                                   <option value="4">4</option>
                                              </select>
                                         </td>
+
                                          <td><button type="button" name="add" id="add" class="btn btn-success">Add More</button></td>  
                                     </tr>  
                                </table>  
                                <input type="button" name="submit" id="submit" class="btn btn-info" value="Submit" />  
                           </div>  
-                     </form>  
+                     </form>
                 </div>  
            </div>  
       </body>  
